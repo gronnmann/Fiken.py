a04b8d56e8aa99a106adb3c8b74b8eb4e57c0836
gronnmann <barti.mruk@gmail.com>
2024-04-24 21:04:06 +0200

    bankAccount validation

--- fiken_py/models/bank_account.py
+++ fiken_py/models/bank_account.py
@@ -1,7 +1,7 @@
 from datetime import date
 from typing import Optional, ClassVar
 
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, Field
 
 from fiken_py.fiken_object import FikenObject, T, FikenObjectRequest
 from fiken_py.fiken_types import BankAccountType
@@ -21,20 +21,28 @@ class BankAccount(BankAccountBase, FikenObject):
     _GET_PATH_SINGLE = '/companies/{companySlug}/bankAccounts/{bankAccountId}'
     _GET_PATH_MULTIPLE = '/companies/{companySlug}/bankAccounts/'
 
+    name: Optional[str] = None
+    bankAccountNumber: Optional[str] = None
+    type: Optional[BankAccountType] = None
     bankAccountId: Optional[int] = None
-    accountCode: str = None
-    reconciledBalance: int = None
+    accountCode: Optional[str] = Field(None, pattern=r"^\d{4}(:\d{5})?$")
+    reconciledBalance: Optional[int] = None
     reconciledDate: Optional[date] = None  # TODO - new date type
-
-    # TODO - validate foreign
-
-    # @model_validator(mode='after')
-    # def check_foreign_service(cls, values):
-    #     if values.get('type') == BankAccountType.FOREIGN and not values.get('foreignService'):
-    #         raise ValueError('foreignService must be provided for foreign bank accounts')
 
 
 class BankAccountCreateRequest(BankAccountBase, FikenObjectRequest):
     BASE_CLASS: ClassVar[FikenObject] = BankAccount
 
     _POST_PATH = '/companies/{companySlug}/bankAccounts/'
+
+    @model_validator(mode="after")
+    @classmethod
+    def validate_foreignService(cls, value):
+        if value.type == BankAccountType.FOREIGN:
+            if value.foreignService is None:
+                raise ValueError("foreignService must be set for foreign bank accounts")
+        else:
+            if value.foreignService is not None:
+                raise ValueError("foreignService is only applicable for foreign bank accounts")
+
+        return value
--- test/test_type_validation.py
+++ test/test_type_validation.py
@@ -1,7 +1,8 @@
 import pytest
 from pydantic import ValidationError
 
-from fiken_py.fiken_types import InvoiceLineRequest
+from fiken_py.fiken_types import InvoiceLineRequest, BankAccountType
+from fiken_py.models import BankAccount, BankAccountCreateRequest
 
 
 def test_validate_invoice_line():
@@ -26,3 +27,61 @@ def test_validate_invoice_line():
         incomeAccount="3000",
     )
     assert invoice_line is not None
+
+
+def test_bankaccount_request_foreignservice():
+    bank_account = BankAccountCreateRequest(
+        name="Test Bank Account",
+        bankAccountNumber="12345678901",
+        type=BankAccountType.FOREIGN,
+        foreignService="Test Service",
+    )
+
+    assert bank_account is not None
+
+    bank_account = BankAccountCreateRequest(
+        name="Test Bank Account",
+        bankAccountNumber="12345678901",
+        type=BankAccountType.NORMAL,
+    )
+
+    assert bank_account is not None
+
+    # type normal, but foreignService
+    with pytest.raises(ValidationError):
+        bank_account = BankAccountCreateRequest(
+            name="Test Bank Account",
+            bankAccountNumber="12345678901",
+            type=BankAccountType.NORMAL,
+            foreignService="Test Service",
+        )
+
+    # type foreign, but no foreignService
+    with pytest.raises(ValidationError):
+        bank_account = BankAccountCreateRequest(
+            name="Test Bank Account",
+            bankAccountNumber="12345678901",
+            type=BankAccountType.FOREIGN,
+        )
+
+
+@pytest.mark.parametrize("test_input,valid", [
+    ("3020", True),
+    ("1500:10001", True),
+    ("1", False),
+    ("1234", True),
+    ("12345:", False),
+    ("150010001", False)
+])
+def test_bankaccount_accountcode(test_input, valid):
+    if not valid:
+        with pytest.raises(ValidationError):
+            acc = BankAccount(
+                accountCode=test_input
+            )
+    else:
+        acc = BankAccount(
+            accountCode=test_input
+        )
+        assert acc is not None
+        assert acc.accountCode == test_input
